4f70132c84c5059210d7248bd56452f509cd7051
Luis Augusto <hello@luis.app>
2022-01-10 19:08:25 -0800

    v1.1.1 - reduce to 50% chance of replying to gas pedal and remove welcome message

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -3,6 +3,7 @@ import type { Event } from './event';
 export const messageCreate: Event<'messageCreate'> = {
   name: 'messageCreate',
   async execute(message) {
+    if (Math.random() > 0.5) return;
     if (!message.content.includes('GAS PEDAL') || message.author.bot) return;
 
     const exclamationCount = Math.floor(Math.random() * 10) + 3;
