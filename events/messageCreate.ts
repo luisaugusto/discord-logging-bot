5dec4ddb2e71bed14bd5a9f3b71078a0a9e0c12e
Luis Augusto <hello@luis.app>
2023-07-11 16:25:36 -0700

    revert back to gpt 3.5

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -23,7 +23,7 @@ export const messageCreate: Event<"messageCreate"> = {
 
     try {
       const openAIResponse = await openai.createChatCompletion({
-        model: "gpt-4",
+        model: "gpt-3.5-turbo",
         messages: [
           {
             role: "system",
